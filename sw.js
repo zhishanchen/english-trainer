e4a683363372e9699067c443ca3482d4fcd9958c
zhishanchen <zhishan118@gmail.com>
2026-04-22 12:34:23 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,18 @@
+var CACHE = 'et-v1';
+var FILES = ['/', '/index.html'];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE).then(function(cache) {
+      return cache.addAll(FILES);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
